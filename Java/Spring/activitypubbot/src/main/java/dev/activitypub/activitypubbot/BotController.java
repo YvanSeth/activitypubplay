bcaa6508f1dfa5876da595c6577b081479d134e9
Yvan <yvan@funnyfarm.bogalow>
2025-01-29 11:45:21 +0000

    add links to the bot list, and better name the template, and add a bot details viewing page

--- Java/Spring/activitypubbot/src/main/java/dev/activitypub/activitypubbot/BotController.java
+++ Java/Spring/activitypubbot/src/main/java/dev/activitypub/activitypubbot/BotController.java
@@ -3,6 +3,7 @@ package dev.activitypub.activitypubbot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -10,7 +11,7 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * This handles requests relating to our bots (users) - so things like
+ * This handles web requests relating to our bots (users) - so things like
  * /@username and /users/username.
  */
 @Controller
@@ -22,17 +23,27 @@ public class BotController {
 
     @GetMapping("/viewbot")
     public String listAll(Model model) {
-        log.info("WebHandler::viewbot");
+        log.info("BotController::viewbot");
 
         List<Bot> botlist = botServ.findAll();
         model.addAttribute("bots", botlist);
+
+        return "listbots";
+    }
+
+    @GetMapping("/viewbot/{username}")
+    public String listAll(@PathVariable String username, Model model) {
+        log.info("BotController::viewbot/" + username);
+
+        Bot bot = botServ.getBotByUsername(username);
+        model.addAttribute("bot", bot);
 
         return "viewbot";
     }
 
     @GetMapping("/makebot")
     public String makebotget(Model model) {
-        log.info("WebHandler::makebot");
+        log.info("BotController::makebot");
 
         Bot bot = new Bot();
         model.addAttribute("bot", bot);
